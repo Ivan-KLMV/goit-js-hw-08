8cfa87c5591371b18156ac3240f8d0c7d96523c6
Ivan Klimov <klimov128@gmail.com>
2023-02-27 18:04:20 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
-
+import Player from '@vimeo/player';
+import lodash from 'lodash.throttle';
+const player = new Player('vimeo-player');
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+player.on('play', function () {
+  console.log('played the video!');
+});
+player.on('timeupdate', lodash(onPlay, 1000));
+function onPlay(data) {
+  console.log(data);
+  // data is an object containing properties specific to that event
+}
