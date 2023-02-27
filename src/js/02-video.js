0450001087b995f9a828407318eaa9c1bbbabe52
Ivan Klimov <klimov128@gmail.com>
2023-02-27 18:59:22 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,32 @@
 import Player from '@vimeo/player';
 import lodash from 'lodash.throttle';
 const player = new Player('vimeo-player');
-const iframe = document.querySelector('iframe');
-console.log(iframe);
-player.on('play', function () {
-  console.log('played the video!');
-});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(Number(currentTime));
+
 player.on('timeupdate', lodash(onPlay, 1000));
+
 function onPlay(data) {
-  console.log(data);
-  // data is an object containing properties specific to that event
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
 }
+
+player
+  .setCurrentTime(Number(currentTime))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
